2c84bcd3766e8a8c01519361c91ed8b51dfd0e74
mrbrunelli <matheus.brunelli@gmail.com>
2022-02-05 06:30:11 -0300

    test: ensure validate returns true if fields match in provided object

---
+++ index.spec.js
@@ -0,0 +1,32 @@
+const { validate } = require('./index')
+
+const responseExample = {
+  status: 200,
+  data: {
+    serviceName: "DatasetSP.save",
+    error: "",
+    status: "1",
+    pendingPrinting: "false",
+    transactionId: "6788329C5763C2982A9537E6DD1D122D",
+    responseBody: {
+      total: "1",
+      result: [["N"]],
+      entities: {
+        entity: {
+          total: {}
+        }
+      }
+    }
+  }
+};
+
+describe('validate', () => {
+  test('should returns true if fields matchs in provided object', () => {
+    const isValid = validate(responseExample, [
+      "status",
+      "data.status",
+      "data.responseBody.entities.entity.total"
+    ])
+    expect(isValid).toBeTruthy()
+  })
+});
