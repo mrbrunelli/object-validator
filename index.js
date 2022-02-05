56bcf2d515f7ab390ffb07aebb5f3426600dae08
mrbrunelli <matheus.brunelli@gmail.com>
2022-02-05 06:29:52 -0300

    feat: ensure validate returns true if fields match in provided object

--- index.js
+++ index.js
@@ -4,41 +4,17 @@ const validate = (object, fields = []) => {
     const [splicedField] = splitedFields.splice(0, 1);
     const joinedFields = splitedFields.join(".");
 
-    const existsFields = !!joinedFields;
+    const existsMoreFields = !!joinedFields;
     const existsFieldInObject = splicedField in object;
 
-    if (existsFieldInObject && existsFields) {
+    if (existsFieldInObject && existsMoreFields) {
       return validate(object[splicedField], [joinedFields]);
     }
 
-    return existsFieldInObject && !existsFields;
+    return existsFieldInObject && !existsMoreFields;
   });
 };
 
-const responseExample = {
-  status: 200,
-  data: {
-    serviceName: "DatasetSP.save",
-    error: "",
-    status: "1",
-    pendingPrinting: "false",
-    transactionId: "6788329C5763C2982A9537E6DD1D122D",
-    responseBody: {
-      total: "1",
-      result: [["N"]],
-      entities: {
-        entity: {
-          total: {}
-        }
-      }
-    }
-  }
-};
-
-const isValid = validate(responseExample, [
-  "status",
-  "data.status",
-  "data.responseBody.entities.entity.total"
-]);
-
-console.log(isValid);
+module.exports = {
+  validate
+}
