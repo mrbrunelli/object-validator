466591563c4964c142544dba2bef15c1ccb4303d
mrbrunelli <matheus.brunelli@gmail.com>
2022-02-05 07:35:21 -0300

    refactor: organize code

--- lib.spec.js
+++ lib.spec.js
@@ -1,31 +1,43 @@
-const { validate } = require('./lib')
+const lib = require('./lib')
 
-const responseExample = {
-  status: 200,
-  data: {
-    serviceName: 'DatasetSP.save',
-    error: '',
-    status: '1',
-    pendingPrinting: 'false',
-    transactionId: '6788329C5763C2982A9537E6DD1D122D',
-    responseBody: {
-      total: '1',
-      result: [['N']],
-      entities: {
-        entity: {
-          total: {}
+const makeObjectStub = () => {
+  return {
+    status: 200,
+    data: {
+      serviceName: 'DatasetSP.save',
+      error: '',
+      status: '1',
+      pendingPrinting: 'false',
+      transactionId: '6788329C5763C2982A9537E6DD1D122D',
+      responseBody: {
+        total: '1',
+        entities: {
+          entity: {}
         }
       }
     }
   }
 }
 
+const makeSut = () => lib
+
 describe('validate', () => {
-  test('should returns true if fields matchs in provided object', () => {
-    const isValid = validate(responseExample, [
+  /**
+   * @type {lib}
+   */
+  let sut
+  let objectStub
+
+  beforeEach(() => {
+    sut = makeSut()
+    objectStub = makeObjectStub()
+  })
+
+  test('should return true if provided fields match with object', () => {
+    const isValid = sut.validate(objectStub, [
       'status',
       'data.status',
-      'data.responseBody.entities.entity.total'
+      'data.responseBody.entities.entity'
     ])
     expect(isValid).toBeTruthy()
   })
